fbcbb73a769002c7e635a30d73443c7ca3c5b16a
zjwzgr <812074418@qq.com>
2016-12-07 20:53:14 +0800

    zhl

--- src/js/index.js
+++ src/js/index.js
@@ -90,7 +90,7 @@ class App extends Component{
 			<div>
 				<h1>{store.getState()}</h1>
 				<button onClick={()=>{ store.dispatch({type:'INCREASE'})}}>+</button>
-				<button onClick={()=>{ store.dispatch({type:'INCREASE'})}}>-</button>
+				<button onClick={()=>{ store.dispatch({type:'DECREASE'})}}>-</button>
 			</div>
 		)
 	}
